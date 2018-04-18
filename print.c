57712c80e257fc2efe76678ba7ef4a231655d7bc
a-yu-relativity <ayu@kcura.com>
2018-04-18 13:14:36 -0500

    Add default case

--- print.c
+++ print.c
@@ -35,6 +35,7 @@ void print_file_contents(char *filename)
                     printf("\\");
                     break;
                 default:
+                    printf("\\");
                     break;
             }
         }
